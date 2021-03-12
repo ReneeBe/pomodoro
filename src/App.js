d5b7c8ef82d920f7d36df716f1d16620b219c521
ReneeBe <reneelberger@gmail.com>
2021-03-11 16:08:50 -0800

    Create basic pomodoro web app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,78 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import IntervalForm from './IntervalForm';
+let longPomoTimer;
+let shortPomoTimer;
 
 function App() {
+  const [longIntervalMinutes, setLongIntervalMinutes] = useState(25);
+  const [shortIntervalMinutes, setShortIntervalMinutes] = useState(5)
+
+  const [remainingTimeSeconds, setRemainingTimeSeconds] = useState(null);
+
+  function handleStartClick() {
+    let remainingSeconds = longIntervalMinutes * 60;
+    longPomoTimer = setInterval(() => {
+      remainingSeconds--;
+      setRemainingTimeSeconds(remainingSeconds);
+      if (remainingSeconds <= 0) {
+        alert("The timer is complete! Take a break")
+        clearInterval(longPomoTimer);
+        handleShortInterval()
+      }
+    }, 1000);
+    
+  }
+
+  function handleShortInterval() {
+    let remainingSeconds = shortIntervalMinutes * 60;
+    shortPomoTimer = setInterval(() => {
+
+      remainingSeconds--;
+      setRemainingTimeSeconds(remainingSeconds);
+      if (remainingTimeSeconds <= 0) {
+        alert("Head back to work - click start when you're ready to start a new timer!")
+        clearInterval(shortPomoTimer);
+      }
+    }, 1000);
+  }
+
+  function handleChange(e) {
+    e.preventDefault();
+    if (e.target.name === 'longTimer') {
+      setLongIntervalMinutes(e.target.value);
+    } 
+    if (e.target.name === 'shortTimer') {
+      setShortIntervalMinutes(e.target.value);
+    }
+  }
+
+  function handleSubmitChangeTimerInterval(e) {
+    e.preventDefault();
+    console.log('renee come back');
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <p> {
+          remainingTimeSeconds ?
+          `${Math.floor(remainingTimeSeconds / 60)} : ${remainingTimeSeconds % 60}`
+          : '25:00' }
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <div >
+        <IntervalForm 
+          handleSubmitChangeTimerInterval = {handleSubmitChangeTimerInterval}
+          handleChange = {handleChange}
+          longIntervalMinutes = {longIntervalMinutes}
+          shortIntervalMinutes = {shortIntervalMinutes}
+        />
+        <button onClick={handleStartClick}>
+          <label>Start the timer!</label>
+        </button>
+      </div>
+      
     </div>
   );
 }
---
+++ src/IntervalForm.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import './App.css';
+
+const IntervalForm = props => {
+    const {handleSubmitChangeTimerInterval, handleChange, longIntervalMinutes, shortIntervalMinutes } = props
+
+    return (
+        <div>
+        <p>Personalize your intervals here (in minutes!):</p>
+        <form onSubmit={handleSubmitChangeTimerInterval}>
+            <label>Work Timer</label>
+                <input 
+                    type="text"
+                    name="longTimer"
+                    value={longIntervalMinutes}
+                    onChange={handleChange}
+                ></input>
+            <label>Break Timer</label>
+                <input
+                    type="test"
+                    name="shortTimer"
+                    value={shortIntervalMinutes}
+                    onChange={handleChange}
+                ></input>
+            <button type="submit">Submit</button>
+        </form>
+        </div>
+    )
+}
+
+export default IntervalForm;
+
+
